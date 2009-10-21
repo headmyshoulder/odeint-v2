566906ccdd8f3b9cd815d7ae09dcaa3a70c8141c
karsten <karsten@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2009-10-21 14:18:00 +0000

    first version of euler stepper

---
+++ boost/numeric/odeint.hpp
@@ -0,0 +1,20 @@
+/* Boost odeint.hpp header file
+ 
+ Copyright 2009 Karsten Ahnert
+ Copyright 2009 Mario Mulansky
+ Copyright 2009 Andre Bergner
+ 
+ This file includes *all* headers needed for integration of ordinary differential equations.
+ 
+ 
+ Distributed under the Boost Software License, Version 1.0.
+ (See accompanying file LICENSE_1_0.txt or
+ copy at http://www.boost.org/LICENSE_1_0.txt)
+*/
+
+#ifndef BOOST_NUMERIC_ODEINT_HPP
+#define BOOST_NUMERIC_ODEINT_HPP
+
+#include <boost/numeric/odeint/euler.hpp>
+
+#endif // BOOST_NUMERIC_ODEINT_HPP
