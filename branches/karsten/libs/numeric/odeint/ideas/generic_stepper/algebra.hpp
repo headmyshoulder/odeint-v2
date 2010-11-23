73fa0cafb1c4d4453b2067fc42bd23c46b0331db
mariomulansky <mariomulansky@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-11-23 09:44:25 +0000

    now using arrays

--- branches/karsten/libs/numeric/odeint/ideas/generic_stepper/algebra.hpp
+++ branches/karsten/libs/numeric/odeint/ideas/generic_stepper/algebra.hpp
@@ -10,6 +10,8 @@
 
 #include <vector>
 
+#include <boost/array.hpp>
+
 #include <boost/numeric/odeint/algebra/standard_algebra.hpp>
 #include <boost/numeric/odeint/algebra/standard_operations.hpp>
 
@@ -22,7 +24,8 @@ struct algebra
 	typedef typename boost::numeric::odeint::standard_operations< double > std_op;
 
 
-	static void foreach( state_type &x_tmp , const state_type &x , const std::vector< double > &a , const state_type *k_vector , const double dt )
+	static void foreach( state_type &x_tmp , const state_type &x , const boost::array< double , n > &a ,
+                            const state_type *k_vector , const double dt )
 	{}
 
 };
@@ -34,7 +37,8 @@ struct algebra< state_type , 1 >
 	typedef typename boost::numeric::odeint::standard_algebra< state_type > std_algebra;
 	typedef typename boost::numeric::odeint::standard_operations< double > std_op;
 
-	static void foreach( state_type &x_tmp , const state_type &x , const std::vector< double > &a , const state_type *k_vector , const double dt )
+	static void foreach( state_type &x_tmp , const state_type &x , const boost::array< double , 1 > &a ,
+                            const state_type *k_vector , const double dt )
 	{
 		std_algebra::for_each3( x_tmp , x ,  k_vector[0] , std_op::scale_sum2( 1.0 , a[0]*dt ) );
 	}
@@ -49,7 +53,8 @@ struct algebra< state_type , 2 >
 	typedef typename boost::numeric::odeint::standard_algebra< state_type > std_algebra;
 	typedef typename boost::numeric::odeint::standard_operations< double > std_op;
 
-	static void foreach( state_type &x_tmp , const state_type &x , const std::vector< double > &a , const state_type *k_vector , const double dt )
+	static void foreach( state_type &x_tmp , const state_type &x , const boost::array< double , 2 > &a ,
+                            const state_type *k_vector , const double dt )
 	{
 		std_algebra::for_each4( x_tmp , x ,  k_vector[0] , k_vector[1] , std_op::scale_sum3( 1.0 , a[0]*dt , a[1]*dt ) );
 	}
