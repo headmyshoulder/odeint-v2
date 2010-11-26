5502135498f3bb79822da59cf41f754a9b6f2292
karsten <karsten@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-11-26 16:49:44 +0000

    * generic stepper typedefs and type construction

--- branches/karsten/libs/numeric/odeint/ideas/generic_stepper/runge_kutta_stepper.hpp
+++ branches/karsten/libs/numeric/odeint/ideas/generic_stepper/runge_kutta_stepper.hpp
@@ -1,4 +1,3 @@
-
 #include <boost/mpl/vector.hpp>
 #include <boost/mpl/size.hpp>
 #include <boost/mpl/push_back.hpp>
@@ -9,31 +8,51 @@
 #include <boost/mpl/at.hpp>
 #include <boost/mpl/copy.hpp>
 #include <boost/mpl/insert_range.hpp>
+#include <boost/mpl/size_t.hpp>
+
+#include <boost/fusion/container.hpp>
+#include <boost/fusion/algorithm.hpp>
+#include <boost/fusion/sequence.hpp>
+#include <boost/fusion/adapted.hpp>
+
 
 #include <vector>
 #include <algorithm>
 #include <iostream>
-
-#include <boost/fusion/container.hpp>
-#include <boost/fusion/algorithm.hpp>
-#include <boost/fusion/include/mpl.hpp>
-#include <boost/fusion/view/zip_view.hpp>
-#include <boost/fusion/include/zip_view.hpp>
-#include <boost/fusion/algorithm/transformation/push_back.hpp>
-#include <boost/fusion/include/push_back.hpp>
-
+#include <string>
 
 #include <boost/array.hpp>
-
 #include <typeinfo>
 
-
-#include "algebra.hpp"
 
 namespace mpl = boost::mpl;
 namespace fusion = boost::fusion;
 
 using namespace std;
+
+
+struct first_stage {};
+struct intermediate_stage {};
+struct last_stage {};
+
+ostream& operator<<( ostream& out , const first_stage& )
+{
+	out << "first stage";
+	return out;
+}
+
+ostream& operator<<( ostream& out , const intermediate_stage& )
+{
+	out << "intermediate stage";
+	return out;
+}
+
+ostream& operator<<( ostream& out , const last_stage& )
+{
+	out << "last stage";
+	return out;
+}
+
 
 
 template< class T , class Constant >
@@ -42,16 +61,11 @@ struct array_wrapper
     typedef typename boost::array< T , Constant::value > type;
 };
 
-struct first_stage {};
-struct intermediate_stage {};
-struct last_stage {};
 
 template< typename T , size_t N , typename StageCategory >
 struct stage_fusion
 {
-    //typedef typename fusion::result_of::as_vector< mpl::vector< size_t , T , boost::array< T , N > , StageCategory > >::type type;
     typedef fusion::vector4< size_t , T , boost::array< T , N > , StageCategory > type;
-    typedef fusion::vector4< size_t , T , boost::array< T , N > , StageCategory >& ref_type;
 };
 
 template< class T , class Constant , class StageCategory >
@@ -72,14 +86,56 @@ struct print_stage
     }
 };
 
+struct print_sequence
+{
+	size_t m_indent;
 
-/* Runge Kutta Stepper - consisting of several stages */
+	print_sequence( size_t indent = 0 )
+	: m_indent( indent ) { }
 
-template< typename State , size_t stage_count >
+	template< typename T >
+	void operator()( T ) const
+	{
+		string in;
+		for( size_t i=0 ; i<m_indent ; ++i ) in += "  ";
+		cout << in << typeid(T).name() << endl;
+	}
+};
+
+struct print_values
+{
+	size_t m_indent;
+
+	print_values( size_t indent = 0 )
+	: m_indent( indent ) { }
+
+	template< typename T >
+	void operator()( const T &t ) const
+	{
+		typedef typename fusion::traits::is_sequence< T >::type seq_type;
+		print_val( t , seq_type() );
+	}
+
+	template< class R >
+	void print_val( const R &r , mpl::false_ ) const
+	{
+		string in;
+		for( size_t i=0 ; i<m_indent ; ++i ) in += "  ";
+		cout << in << r << endl;
+	}
+
+	template< class S >
+	void print_val( const S &s , mpl::true_ ) const
+	{
+		fusion::for_each( s , print_values( m_indent + 1 ) );
+	}
+};
+
+
+template< size_t stage_count >
 class runge_kutta_stepper
 {
 public:
-    typedef State state_type;
 
     typedef mpl::range_c< size_t , 1 , stage_count > stage_indices;
 
@@ -97,89 +153,60 @@ public:
     >::type coef_a_type;
 
     typedef boost::array< double , stage_count > coef_b_type;
-
     typedef boost::array< double , stage_count > coef_c_type;
 
 
-    typedef //typename fusion::result_of::as_vector<
-        typename mpl::copy<
+    typedef typename mpl::copy
+        <
             stage_indices,
             mpl::inserter
-              <
+            <
                 mpl::vector0<> ,
                 mpl::push_back< mpl::_1 , stage_fusion_wrapper< double , mpl::_2 , intermediate_stage > >
-              >
-            >::type stage_vector_base0;
-        //>::type
+            >
+        >::type base_of_stage_vector_base;
 
-    typedef typename fusion::result_of::as_vector<
-        typename mpl::push_back<
-            stage_vector_base0 ,
-            typename stage_fusion_wrapper< double , mpl::integral_c< size_t , stage_count > , last_stage >::type
+    typedef typename fusion::result_of::as_vector
+    <
+        typename mpl::push_back
+        <
+            base_of_stage_vector_base ,
+            typename stage_fusion_wrapper< double , mpl::size_t< stage_count > , last_stage >::type
         >::type
     >::type stage_vector_base;
 
-
-    struct stage_vector_type : public stage_vector_base
+    struct stage_vector : public stage_vector_base
     {
-        struct init_stage{
+    	struct do_insertion
+    	{
+    		stage_vector_base &m_base;
+    		const coef_a_type &m_a;
+    		const coef_c_type &m_c;
 
-            const coef_c_type &m_c;
+    		do_insertion( stage_vector_base &base , const coef_a_type &a , const coef_c_type &c )
+    		: m_base( base ) , m_a( a ) , m_c( c ) { }
 
-            init_stage( const coef_b_type &b , const coef_c_type &c )
-                : m_c( c ) {}
+    		template< class Index >
+    		void operator()( Index ) const
+    		{
+    			fusion::at_c< 0 >( fusion::at< Index >( m_base ) ) = Index::value;
+    			fusion::at_c< 1 >( fusion::at< Index >( m_base ) ) = m_c[ Index::value ];
+    			fusion::at_c< 2 >( fusion::at< Index >( m_base ) ) = fusion::at< Index >( m_a );
+    		}
+    	};
 
-
-            template< typename T , size_t N , typename StageCategory>
-            void operator() (
-                    fusion::vector2< fusion::vector4< size_t , T , boost::array< T , N > , StageCategory >& ,
-                                     //stage_fusion< T , N , StageCategory >::ref_type ,
-                                     boost::array< T , N > > stage ) const
-            {
-                std::cout << N << std::endl;
-                fusion::at_c<0>( fusion::at_c<0>(stage) ) = N;
-                fusion::at_c<1>( fusion::at_c<0>(stage) ) = m_c[N-1];
-                fusion::at_c<2>( fusion::at_c<0>(stage) ) = fusion::at_c<1>( stage );
-            }
-
-        };
-
-
-        stage_vector_type( const coef_a_type &a , const coef_b_type &b , const coef_c_type &c )
-        {
-            //typedef typename fusion::result_of::push_back< const coef_a_type , const coef_b_type >::type ab_type;
-            typedef fusion::joint_view< const coef_a_type , const fusion::single_view< coef_b_type > > ab_type;
-            const ab_type ab = fusion::push_back( a , b );
-            // iterate stage and c in parallel
-            typedef fusion::vector< stage_vector_base& , const ab_type& > zipped_stage;
-
-            fusion::for_each( fusion::zip_view<zipped_stage>( zipped_stage( static_cast< stage_vector_base& >( *this ) , ab ) ) ,
-                              init_stage( b , c ) );
-        }
+    	stage_vector( const coef_a_type &a , const coef_b_type &b , const coef_c_type &c )
+    	{
+    		typedef mpl::range_c< size_t , 0 , stage_count - 1 > indices;
+    		mpl::for_each< indices >( do_insertion( *this , a , c ) );
+			fusion::at_c< 0 >( fusion::at_c< stage_count - 1 >( *this ) ) = stage_count - 1 ;
+			fusion::at_c< 1 >( fusion::at_c< stage_count - 1 >( *this ) ) = c[ stage_count - 1 ];
+			fusion::at_c< 2 >( fusion::at_c< stage_count - 1 >( *this ) ) = b;
+    	}
     };
 
 
-//    template< class System >
-//    struct calculate_stage
-//    {
-//        System &system;
-//        state_type &x , &x_tmp;
-//        state_type *k_vector;
-//        const double t;
-//        const double dt;
-//
-//        calculate_stage( System &_system , state_type &_x , state_type &_x_tmp , state_type *_k_vector ,
-//                            const double _t , const double _dt )
-//        : system( _system ) , x( _x ) , x_tmp( _x_tmp ) , k_vector( _k_vector ) , t( _t ) , dt( _dt )
-//        {}
-//
-//        template< class Stage >
-//        void operator()( Stage const &stage ) const
-//        {
-//            stage( system , x_tmp , x , k_vector , t , dt );
-//        }
-//
-//    };
+
 
 
 public:
@@ -187,26 +214,36 @@ public:
     runge_kutta_stepper( const coef_a_type &a ,
                             const coef_b_type &b ,
                             const coef_c_type &c )
-        : m_stages( a , b , c )
+    : m_a( a ) , m_b( b ) , m_c( c ) ,
+      m_stages( a , b , c )
+
     { }
 
-    template< class System >
-    void do_step( System &system , state_type &x , double t , const double dt )
-    {
-//        fusion::for_each( m_stages , calculate_stage< System >( system , x , m_x_tmp , m_k_vector , t , dt ) );
-//        m_last_stage( system , m_x_tmp , x , m_k_vector , t , dt );
-    }
 
     void print_vals()
     {
-        fusion::for_each( m_stages , print_stage() );
+    	cout << "Typeof state_vector_base : " << endl;
+    	mpl::for_each< stage_vector_base >( print_sequence( 1 ) );
+
+    	cout << "m_a : " << endl;
+    	fusion::for_each( m_a , print_values( 1 ) );
+
+    	cout << "m_b : " << endl;
+    	fusion::for_each( m_b , print_values( 1 ) );
+
+    	cout << "m_c : " << endl;
+    	fusion::for_each( m_c , print_values( 1 ) );
+
+    	cout << "m_stages : " << endl;
+    	fusion::for_each( m_stages , print_values( 1 ) );
     }
+
 
 
 private:
 
-    state_type m_k_vector[stage_count];
-    state_type m_x_tmp;
-    const stage_vector_type m_stages;
-//    last_stage_type m_last_stage;
+    const coef_a_type m_a;
+    const coef_b_type m_b;
+    const coef_c_type m_c;
+    const stage_vector m_stages;
 };
