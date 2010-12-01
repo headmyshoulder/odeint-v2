fa3d1da40a753e02d0b07bbf817ef8d0db03a48f
mariomulansky <mariomulansky@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-12-01 15:52:17 +0000

    rk fusion working performance odeint, mpl , fusion

--- branches/karsten/libs/numeric/odeint/ideas/butcher/algebra.hpp
+++ branches/karsten/libs/numeric/odeint/ideas/butcher/algebra.hpp
@@ -28,8 +28,8 @@ struct algebra
 template< class state_type , class coef_type >
 struct algebra< state_type , coef_type , mpl::int_< 0 > >
 {
-	typedef typename boost::numeric::odeint::standard_algebra< state_type > std_algebra;
-	typedef typename boost::numeric::odeint::standard_operations< double > std_op;
+	typedef boost::numeric::odeint::standard_algebra std_algebra;
+	typedef boost::numeric::odeint::standard_operations< double > std_op;
 
 	typedef typename state_type::iterator iterator;
 	typedef typename state_type::const_iterator const_iterator;
@@ -58,8 +58,8 @@ struct algebra< state_type , coef_type , mpl::int_< 0 > >
 template< class state_type , class coef_type >
 struct algebra< state_type , coef_type , mpl::int_< 1 > >
 {
-	typedef typename boost::numeric::odeint::standard_algebra< state_type > std_algebra;
-	typedef typename boost::numeric::odeint::standard_operations< double > std_op;
+	typedef boost::numeric::odeint::standard_algebra std_algebra;
+	typedef boost::numeric::odeint::standard_operations< double > std_op;
 
 	typedef typename state_type::iterator iterator;
 	typedef typename state_type::const_iterator const_iterator;
@@ -69,10 +69,13 @@ struct algebra< state_type , coef_type , mpl::int_< 1 > >
 		const static double a1 = dt * convert_value< typename mpl::at< coef_type , mpl::int_< 0 > >::type >::get_value();
 		const static double a2 = dt * convert_value< typename mpl::at< coef_type , mpl::int_< 1 > >::type >::get_value();
 
-		iterator first1 = x_tmp.begin() , last1 = x_tmp.end();
-		const_iterator first2 = x.begin() , first3 = k_vector[0].begin() , first4 = k_vector[1].begin();
-		while( first1 != last1 )
-			*first1++ = *first2++ + a1 * *first3++ + a2 * *first4++;
+//		iterator first1 = x_tmp.begin() , last1 = x_tmp.end();
+//		const_iterator first2 = x.begin() , first3 = k_vector[0].begin() , first4 = k_vector[1].begin();
+//		while( first1 != last1 )
+//			*first1++ = *first2++ + a1 * *first3++ + a2 * *first4++;
+
+		std_algebra::for_each4( x_tmp , x ,  k_vector[0] , k_vector[1] ,
+		                std_op::scale_sum3( 1.0 , a1 , a2 ) );
 
 //		for( size_t i=0 ; i<x.size() ; ++i )
 //		{
@@ -86,48 +89,51 @@ struct algebra< state_type , coef_type , mpl::int_< 1 > >
 template< class state_type , class coef_type >
 struct algebra< state_type , coef_type , mpl::int_< 2 > >
 {
-	typedef typename boost::numeric::odeint::standard_algebra< state_type > std_algebra;
-	typedef typename boost::numeric::odeint::standard_operations< double > std_op;
+	typedef boost::numeric::odeint::standard_algebra std_algebra;
+	typedef boost::numeric::odeint::standard_operations< double > std_op;
 
 	typedef typename state_type::iterator iterator;
 	typedef typename state_type::const_iterator const_iterator;
 
 	static void do_step( state_type &x_tmp , const state_type &x , const state_type *k_vector , const double dt )
 	{
-//		const static double a1 = dt * convert_value< typename mpl::at< coef_type , mpl::int_< 0 > >::type >::get_value();
-//		const static double a2 = dt * convert_value< typename mpl::at< coef_type , mpl::int_< 1 > >::type >::get_value();
-//		const static double a3 = dt * convert_value< typename mpl::at< coef_type , mpl::int_< 2 > >::type >::get_value();
+		const static double a1 = dt * convert_value< typename mpl::at< coef_type , mpl::int_< 0 > >::type >::get_value();
+		const static double a2 = dt * convert_value< typename mpl::at< coef_type , mpl::int_< 1 > >::type >::get_value();
+		const static double a3 = dt * convert_value< typename mpl::at< coef_type , mpl::int_< 2 > >::type >::get_value();
 //
 //		iterator first1 = x_tmp.begin() , last1 = x_tmp.end();
 //		const_iterator first2 = x.begin() , first3 = k_vector[0].begin() , first4 = k_vector[1].begin() , first5 = k_vector[2].begin();
 //		while( first1 != last1 )
 //			*first1++ = *first2++ + a1 * *first3++ + a2 * *first4++ + a3 * *first5++;
 
-		for( size_t i=0 ; i<x.size() ; ++i )
-		{
-			x_tmp[i] = x[i] +
-					dt * convert_value< typename mpl::at< coef_type , mpl::int_< 0 > >::type >::get_value() * k_vector[0][i] +
-					dt * convert_value< typename mpl::at< coef_type , mpl::int_< 1 > >::type >::get_value() * k_vector[1][i] +
-					dt * convert_value< typename mpl::at< coef_type , mpl::int_< 2 > >::type >::get_value() * k_vector[2][i];
-		}
+	    std_algebra::for_each5( x_tmp , x ,  k_vector[0] , k_vector[1] , k_vector[2] ,
+	                            std_op::scale_sum4( 1.0 , a1 , a2 , a3 ) );
+
+//		for( size_t i=0 ; i<x.size() ; ++i )
+//		{
+//			x_tmp[i] = x[i] +
+//					dt * convert_value< typename mpl::at< coef_type , mpl::int_< 0 > >::type >::get_value() * k_vector[0][i] +
+//					dt * convert_value< typename mpl::at< coef_type , mpl::int_< 1 > >::type >::get_value() * k_vector[1][i] +
+//					dt * convert_value< typename mpl::at< coef_type , mpl::int_< 2 > >::type >::get_value() * k_vector[2][i];
+//		}
 	}
 };
 
 template< class state_type , class coef_type >
 struct algebra< state_type , coef_type , mpl::int_< 3 > >
 {
-	typedef typename boost::numeric::odeint::standard_algebra< state_type > std_algebra;
-	typedef typename boost::numeric::odeint::standard_operations< double > std_op;
+	typedef boost::numeric::odeint::standard_algebra std_algebra;
+	typedef boost::numeric::odeint::standard_operations< double > std_op;
 
 	typedef typename state_type::iterator iterator;
 	typedef typename state_type::const_iterator const_iterator;
 
 	static void do_step( state_type &x_tmp , const state_type &x , const state_type *k_vector , const double dt )
 	{
-//		const static double a1 = dt * convert_value< typename mpl::at< coef_type , mpl::int_< 0 > >::type >::get_value();
-//		const static double a2 = dt * convert_value< typename mpl::at< coef_type , mpl::int_< 1 > >::type >::get_value();
-//		const static double a3 = dt * convert_value< typename mpl::at< coef_type , mpl::int_< 2 > >::type >::get_value();
-//		const static double a4 = dt * convert_value< typename mpl::at< coef_type , mpl::int_< 3 > >::type >::get_value();
+		const static double a1 = dt * convert_value< typename mpl::at< coef_type , mpl::int_< 0 > >::type >::get_value();
+		const static double a2 = dt * convert_value< typename mpl::at< coef_type , mpl::int_< 1 > >::type >::get_value();
+		const static double a3 = dt * convert_value< typename mpl::at< coef_type , mpl::int_< 2 > >::type >::get_value();
+		const static double a4 = dt * convert_value< typename mpl::at< coef_type , mpl::int_< 3 > >::type >::get_value();
 //
 //		iterator first1 = x_tmp.begin() , last1 = x_tmp.end();
 //		const_iterator first2 = x.begin() , first3 = k_vector[0].begin() , first4 = k_vector[1].begin() , first5 = k_vector[2].begin();
@@ -135,33 +141,36 @@ struct algebra< state_type , coef_type , mpl::int_< 3 > >
 //		while( first1 != last1 )
 //			*first1++ = *first2++ + a1 * *first3++ + a2 * *first4++ + a3 * *first5++ + a4 * *first6++;
 
-		for( size_t i=0 ; i<x.size() ; ++i )
-		{
-			x_tmp[i] = x[i] +
-					dt * convert_value< typename mpl::at< coef_type , mpl::int_< 0 > >::type >::get_value() * k_vector[0][i] +
-					dt * convert_value< typename mpl::at< coef_type , mpl::int_< 1 > >::type >::get_value() * k_vector[1][i] +
-					dt * convert_value< typename mpl::at< coef_type , mpl::int_< 2 > >::type >::get_value() * k_vector[2][i] +
-					dt * convert_value< typename mpl::at< coef_type , mpl::int_< 3 > >::type >::get_value() * k_vector[3][i];
-		}
+	    std_algebra::for_each6( x_tmp , x ,  k_vector[0] , k_vector[1] , k_vector[2] , k_vector[3] ,
+	                                    std_op::scale_sum5( 1.0 , a1 , a2 , a3 , a4 ) );
+
+//		for( size_t i=0 ; i<x.size() ; ++i )
+//		{
+//			x_tmp[i] = x[i] +
+//					dt * convert_value< typename mpl::at< coef_type , mpl::int_< 0 > >::type >::get_value() * k_vector[0][i] +
+//					dt * convert_value< typename mpl::at< coef_type , mpl::int_< 1 > >::type >::get_value() * k_vector[1][i] +
+//					dt * convert_value< typename mpl::at< coef_type , mpl::int_< 2 > >::type >::get_value() * k_vector[2][i] +
+//					dt * convert_value< typename mpl::at< coef_type , mpl::int_< 3 > >::type >::get_value() * k_vector[3][i];
+//		}
 	}
 };
 
 template< class state_type , class coef_type >
 struct algebra< state_type , coef_type , mpl::int_< 4 > >
 {
-	typedef typename boost::numeric::odeint::standard_algebra< state_type > std_algebra;
-	typedef typename boost::numeric::odeint::standard_operations< double > std_op;
+	typedef boost::numeric::odeint::standard_algebra std_algebra;
+	typedef boost::numeric::odeint::standard_operations< double > std_op;
 
 	typedef typename state_type::iterator iterator;
 	typedef typename state_type::const_iterator const_iterator;
 
 	static void do_step( state_type &x_tmp , const state_type &x , const state_type *k_vector , const double dt )
 	{
-//		const static double a1 = dt * convert_value< typename mpl::at< coef_type , mpl::int_< 0 > >::type >::get_value();
-//		const static double a2 = dt * convert_value< typename mpl::at< coef_type , mpl::int_< 1 > >::type >::get_value();
-//		const static double a3 = dt * convert_value< typename mpl::at< coef_type , mpl::int_< 2 > >::type >::get_value();
-//		const static double a4 = dt * convert_value< typename mpl::at< coef_type , mpl::int_< 3 > >::type >::get_value();
-//		const static double a5 = dt * convert_value< typename mpl::at< coef_type , mpl::int_< 4 > >::type >::get_value();
+		const static double a1 = dt * convert_value< typename mpl::at< coef_type , mpl::int_< 0 > >::type >::get_value();
+		const static double a2 = dt * convert_value< typename mpl::at< coef_type , mpl::int_< 1 > >::type >::get_value();
+		const static double a3 = dt * convert_value< typename mpl::at< coef_type , mpl::int_< 2 > >::type >::get_value();
+		const static double a4 = dt * convert_value< typename mpl::at< coef_type , mpl::int_< 3 > >::type >::get_value();
+		const static double a5 = dt * convert_value< typename mpl::at< coef_type , mpl::int_< 4 > >::type >::get_value();
 //
 //		iterator first1 = x_tmp.begin() , last1 = x_tmp.end();
 //		const_iterator first2 = x.begin() , first3 = k_vector[0].begin() , first4 = k_vector[1].begin() , first5 = k_vector[2].begin();
@@ -169,15 +178,19 @@ struct algebra< state_type , coef_type , mpl::int_< 4 > >
 //		while( first1 != last1 )
 //			*first1++ = *first2++ + a1 * *first3++ + a2 * *first4++ + a3 * *first5++ + a4 * *first6++ + a5 * *first7++;
 
-		for( size_t i=0 ; i<x.size() ; ++i )
-		{
-			x_tmp[i] = x[i] +
-					dt * convert_value< typename mpl::at< coef_type , mpl::int_< 0 > >::type >::get_value() * k_vector[0][i] +
-					dt * convert_value< typename mpl::at< coef_type , mpl::int_< 1 > >::type >::get_value() * k_vector[1][i] +
-					dt * convert_value< typename mpl::at< coef_type , mpl::int_< 2 > >::type >::get_value() * k_vector[2][i] +
-					dt * convert_value< typename mpl::at< coef_type , mpl::int_< 3 > >::type >::get_value() * k_vector[3][i] +
-					dt * convert_value< typename mpl::at< coef_type , mpl::int_< 4 > >::type >::get_value() * k_vector[4][i];
-		}
+        std_algebra::for_each7( x_tmp , x ,  k_vector[0] , k_vector[1] , k_vector[2] , k_vector[3] , k_vector[4] ,
+                                        std_op::scale_sum6( 1.0 , a1 , a2 , a3 , a4 , a5 ) );
+
+
+//		for( size_t i=0 ; i<x.size() ; ++i )
+//		{
+//			x_tmp[i] = x[i] +
+//					dt * convert_value< typename mpl::at< coef_type , mpl::int_< 0 > >::type >::get_value() * k_vector[0][i] +
+//					dt * convert_value< typename mpl::at< coef_type , mpl::int_< 1 > >::type >::get_value() * k_vector[1][i] +
+//					dt * convert_value< typename mpl::at< coef_type , mpl::int_< 2 > >::type >::get_value() * k_vector[2][i] +
+//					dt * convert_value< typename mpl::at< coef_type , mpl::int_< 3 > >::type >::get_value() * k_vector[3][i] +
+//					dt * convert_value< typename mpl::at< coef_type , mpl::int_< 4 > >::type >::get_value() * k_vector[4][i];
+//		}
 	}
 };
 
