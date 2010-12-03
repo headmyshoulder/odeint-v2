d4286c41177201afbbf5889ae6916553202baf08
mariomulansky <mariomulansky@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-12-03 14:50:08 +0000

    more performance tests

--- branches/karsten/libs/numeric/odeint/ideas/generic_stepper/fusion_algebra.hpp
+++ branches/karsten/libs/numeric/odeint/ideas/generic_stepper/fusion_algebra.hpp
@@ -15,28 +15,29 @@
 #include <boost/numeric/odeint/algebra/standard_algebra.hpp>
 #include <boost/numeric/odeint/algebra/standard_operations.hpp>
 
-template< typename state_type , size_t n >
+template< size_t n >
 struct fusion_algebra
 {
 
 	typedef boost::numeric::odeint::standard_algebra std_algebra;
 	typedef boost::numeric::odeint::standard_operations< double > std_op;
 
-
-	static void foreach( state_type &x_tmp , const state_type &x , const boost::array< double , n > &a ,
+	template< class state_type >
+	inline static void foreach( state_type &x_tmp , const state_type &x , const boost::array< double , n > &a ,
                             const state_type *k_vector , const double dt )
 	{}
 
 };
 
-template< typename state_type>
-struct fusion_algebra< state_type , 1 >
+template<>
+struct fusion_algebra< 1 >
 {
 
 	typedef boost::numeric::odeint::standard_algebra std_algebra;
 	typedef boost::numeric::odeint::standard_operations< double > std_op;
 
-	static void foreach( state_type &x_tmp , const state_type &x , const boost::array< double , 1 > &a ,
+	template< class state_type >
+	inline static void foreach( state_type &x_tmp , const state_type &x , const boost::array< double , 1 > &a ,
                             const state_type *k_vector , const double dt )
 	{
 		std_algebra::for_each3( x_tmp , x ,  k_vector[0] , std_op::scale_sum2( 1.0 , a[0]*dt ) );
@@ -45,14 +46,15 @@ struct fusion_algebra< state_type , 1 >
 };
 
 
-template< typename state_type>
-struct fusion_algebra< state_type , 2 >
+template<>
+struct fusion_algebra< 2 >
 {
 
 	typedef boost::numeric::odeint::standard_algebra std_algebra;
 	typedef boost::numeric::odeint::standard_operations< double > std_op;
 
-	static void foreach( state_type &x_tmp , const state_type &x , const boost::array< double , 2 > &a ,
+	template< class state_type >
+	inline static void foreach( state_type &x_tmp , const state_type &x , const boost::array< double , 2 > &a ,
                             const state_type *k_vector , const double dt )
 	{
 		std_algebra::for_each4( x_tmp , x ,  k_vector[0] , k_vector[1] ,
@@ -62,14 +64,15 @@ struct fusion_algebra< state_type , 2 >
 };
 
 
-template< typename state_type>
-struct fusion_algebra< state_type , 3 >
+template<>
+struct fusion_algebra< 3 >
 {
 
     typedef boost::numeric::odeint::standard_algebra std_algebra;
     typedef boost::numeric::odeint::standard_operations< double > std_op;
 
-    static void foreach( state_type &x_tmp , const state_type &x , const boost::array< double , 3 > &a ,
+    template< class state_type >
+    inline static void foreach( state_type &x_tmp , const state_type &x , const boost::array< double , 3 > &a ,
                             const state_type *k_vector , const double dt )
     {
         std_algebra::for_each5( x_tmp , x ,  k_vector[0] , k_vector[1] , k_vector[2] ,
@@ -80,14 +83,15 @@ struct fusion_algebra< state_type , 3 >
 
 
 
-template< typename state_type>
-struct fusion_algebra< state_type , 4 >
+template<>
+struct fusion_algebra< 4 >
 {
 
     typedef boost::numeric::odeint::standard_algebra std_algebra;
     typedef boost::numeric::odeint::standard_operations< double > std_op;
 
-    static void foreach( state_type &x_tmp , const state_type &x , const boost::array< double , 4 > &a ,
+    template< class state_type >
+    inline static void foreach( state_type &x_tmp , const state_type &x , const boost::array< double , 4 > &a ,
                             const state_type *k_vector , const double dt )
     {
         std_algebra::for_each6( x_tmp , x ,  k_vector[0] , k_vector[1] , k_vector[2] , k_vector[3] ,
