f69ea7622e3029f853287a9e8d72d1df45a2e66e
karsten <karsten@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-01-19 12:48:39 +0000

    * inculding vector_space_reduce.hpp

---
+++ boost/numeric/odeint/algebra/vector_space_reduce.hpp
@@ -0,0 +1,32 @@
+/*
+ * vector_space_reduce.hpp
+ *
+ *  Created on: Jan 19, 2011
+ *      Author: karsten
+ */
+
+#ifndef VECTOR_SPACE_REDUCE_HPP_
+#define VECTOR_SPACE_REDUCE_HPP_
+
+namespace boost {
+namespace numeric {
+namespace odeint {
+
+template< class State > struct vector_space_reduce;
+
+//template< class LorenzState >
+//class vector_space_reduce
+//{
+//	template< class Value , class Op >
+//	Value operator()( const State & , Op op , Value init ) const
+//	{
+//		// ...
+//	}
+//};
+
+} // odeint
+} // numeric
+} // boost
+
+
+#endif /* VECTOR_SPACE_REDUCE_HPP_ */
