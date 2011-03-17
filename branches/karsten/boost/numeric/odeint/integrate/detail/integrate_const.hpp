fdd1057e909ba5f8a0aa9653ad984e4365ace734
karsten <karsten@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-03-17 17:08:41 +0000

    integrate and docs

--- branches/karsten/boost/numeric/odeint/integrate/detail/integrate_const.hpp
+++ branches/karsten/boost/numeric/odeint/integrate/detail/integrate_const.hpp
@@ -11,40 +11,45 @@
 
 #include <boost/numeric/odeint/integrate/detail/integrate_adaptive.hpp>
 
-#include <iostream>
-using namespace std;
-
-
 namespace boost {
 namespace numeric {
 namespace odeint {
 namespace detail {
 
 
+/*
+ * integrates with constant time step using simple steppers
 
+ * No step size control
+ */
 template< class Stepper , class System , class State , class Time , class Observer >
 size_t integrate_const(
 		Stepper stepper , System system , State &start_state ,
 		Time start_time , Time end_time , Time dt ,
-		Observer &observer , stepper_tag
+		Observer observer , stepper_tag
 		)
 {
+	size_t count = 0;
 	while( start_time < end_time )
 	{
-		observer( start_time , start_state );
+		observer( start_state , start_time );
 		stepper.do_step( system , start_state , start_time , dt );
 		start_time += dt;
+		++count;
 	}
-	observer( start_time , start_state );
-	return 0;
+	return count;
 }
 
 
-
+/*
+ * integrates with constant time step using a controlled stepper
+ *
+ * step size control is used
+ */
 template< class Stepper , class System , class State , class Time , class Observer >
 size_t integrate_const(
 		Stepper stepper , System system , State &start_state ,
-		Time &start_time , const Time &end_time , Time &dt ,
+		Time &start_time , Time end_time , Time &dt ,
 		Observer observer , controlled_stepper_tag
 		)
 {
@@ -52,37 +57,41 @@ size_t integrate_const(
 	Time time_step = dt;
 	while( start_time < end_time )
 	{
-		observer( start_time , start_state );
 		Time next_time = start_time + time_step;
 		if( next_time > end_time ) next_time = end_time;
 		detail::integrate_adaptive(
 				stepper , system , start_state , start_time , next_time , dt ,
 				do_nothing_observer() , controlled_stepper_tag() );
 		++count;
+		observer( start_state , start_time );
 	}
-	observer( start_time , start_state );
 	return count;
 }
 
 
 
+/*
+ * integrates with constant time step using a controlled stepper
+ *
+ * step size control is used if the stepper is a controlled stepper, otherwise not
+ *
+ * TODO : check the loops for the observers
+ */
 template< class Stepper , class System , class State , class Time , class Observer >
 size_t integrate_const(
 		Stepper stepper , System system , State &start_state ,
 		Time start_time , Time end_time , Time dt ,
 		Observer observer , dense_output_stepper_tag )
 {
-	clog << "dense output" << endl;
 	stepper.initialize( start_state , start_time , dt );
 
 	size_t count = 0;
-	size_t count2 = 0;
 	while( start_time < end_time )
 	{
 		while( ( start_time < stepper.current_time() ) && ( start_time < end_time ) )
 		{
 			stepper.calc_state( start_time , start_state );
-			observer( start_time , start_state );
+			observer( start_state , start_time );
 			start_time += dt;
 			++count;
 		}
@@ -91,15 +100,9 @@ size_t integrate_const(
 		if( start_time < end_time )
 		{
 			stepper.do_step( system );
-			++count2;
 		}
 	}
-	clog << count2 << endl;
 	return count;
-
-
-	observer( start_time , start_state );
-	return 0;
 }
 
 
