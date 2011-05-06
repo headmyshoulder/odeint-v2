e7005dcec3c362d3199355b091a99b367602ad80
mariomulansky <mariomulansky@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-05-06 11:49:08 +0000

    better graphs

--- libs/numeric/odeint/ideas/fusion_runge_kutta/performance/perf_tests.py
+++ libs/numeric/odeint/ideas/fusion_runge_kutta/performance/perf_tests.py
@@ -1,20 +1,23 @@
 from os import popen
 from os.path import isfile
 
-bin_path = "bin/gcc-4.6/release/"
+#bin_path = "bin/gcc-4.4/release/"
 #bin_path = "bin/intel-linux/release/"
+bin_path = "bin\\msvc-9.0express\\release\\threading-multi\\"
+extension = ".exe"
 
-#bins = [ "odeint_rk4" , "odeint_rk4_def_alg" , "generic_rk4" , "nr_rk4" , "gsl_rk4" , "rt_generic_rk4" , 
-bins = [ "odeint_rk54ck" , "odeint_rk54ck_def_alg" , "generic_rk54ck" , "nr_rk54ck" , "gsl_rk54ck" ]
+bins = [ "odeint_rk4" , "odeint_rk4_def_alg" , "generic_rk4" , "nr_rk4" , "gsl_rk4" , "rt_generic_rk4" , 
+ "odeint_rk54ck" , "odeint_rk54ck_def_alg" , "generic_rk54ck" , "nr_rk54ck" , "gsl_rk54ck" ]
+
 results = []
 
 print "Performance tests for " , bin_path
 print
 
 for bin in bins:
-	if isfile( bin_path + bin ):
+	if isfile( bin_path + bin + extension):
 		print "Running" , bin
-		res = popen( bin_path+bin ).read()
+		res = popen( bin_path+bin+extension ).read()
 		print bin , res
 		results.append( res )
 	else:
