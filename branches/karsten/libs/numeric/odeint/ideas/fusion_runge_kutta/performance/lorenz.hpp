77fce6873613dcc4b73d5a687f510b9e82847736
mariomulansky <mariomulansky@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-05-13 15:43:23 +0000

    better msvc performance

--- branches/karsten/libs/numeric/odeint/ideas/fusion_runge_kutta/performance/lorenz.hpp
+++ branches/karsten/libs/numeric/odeint/ideas/fusion_runge_kutta/performance/lorenz.hpp
@@ -8,6 +8,8 @@
 #ifndef LORENZ_HPP_
 #define LORENZ_HPP_
 
+#include <boost/array.hpp>
+
 struct lorenz
 {
     template< class state_type >
@@ -23,7 +25,9 @@ struct lorenz
 };
 
 
-template< class state_type >
+typedef boost::array< double , 3 > state_type;
+
+
 inline void lorenz_func( const state_type &x , state_type &dxdt , const double t )
 {
     const double sigma = 10.0;
@@ -33,20 +37,5 @@ inline void lorenz_func( const state_type &x , state_type &dxdt , const double t
     dxdt[1] = R * x[0] - x[1] - x[0] * x[2];
     dxdt[2] = x[0]*x[1] - b * x[2];
 }
-
-#include <gsl/gsl_matrix.h>
-
-int lorenz_gsl( const double t , const double y[] , double f[] , void *params)
-{
-    const double sigma = 10.0;
-    const double R = 28.0;
-    const double b = 8.0 / 3.0;
-
-    f[0] = sigma * ( y[1] - y[0] );
-    f[1] = R * y[0] - y[1] - y[0] * y[2];
-    f[2] = y[0]*y[1] - b * y[2];
-    return GSL_SUCCESS;
-}
-
 
 #endif /* LORENZ_HPP_ */
