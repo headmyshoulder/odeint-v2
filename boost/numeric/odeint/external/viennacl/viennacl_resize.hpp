e7bd71b238a901eea65633c1434a309e09c4578b
Denis Demidov <ddemidov@ksu.ru>
2012-08-30 10:57:34 +0400

    Tabs removed

--- boost/numeric/odeint/external/viennacl/viennacl_resize.hpp
+++ boost/numeric/odeint/external/viennacl/viennacl_resize.hpp
@@ -41,7 +41,7 @@ struct resize_impl< viennacl::vector< T > , viennacl::vector< T > >
 {
     static void resize( viennacl::vector< T > &x1 , const viennacl::vector< T > &x2 )
     {
-	x1.resize( x2.size() , false );
+        x1.resize( x2.size() , false );
     }
 };
 
@@ -50,7 +50,7 @@ struct same_size_impl< viennacl::vector< T > , viennacl::vector< T > >
 {
     static bool same_size( const viennacl::vector< T > &x1 , const viennacl::vector< T > &x2 )
     {
-	return x1.size() == x2.size();
+        return x1.size() == x2.size();
     }
 };
 
