9d9cad3d36c2640b16234f9070ca601e0a34161f
Karsten Ahnert <karsten.ahnert@gmx.de>
2012-10-05 07:23:04 +0200

    put some resizer code into detail namespace

--- boost/numeric/odeint/util/resize.hpp
+++ boost/numeric/odeint/util/resize.hpp
@@ -53,28 +53,32 @@ void resize( StateOut &x1 , const StateIn &x2 )
     resize_impl< StateOut , StateIn >::resize( x1 , x2 );
 }
 
-struct resizer
-{
-    typedef void result_type;
 
-    template< class StateOut , class StateIn >
-    void operator()( StateOut &x1 , const StateIn &x2 ) const
+namespace detail {
+
+    struct resizer
     {
-        resize_op( x1 , x2 , typename is_resizeable< StateOut >::type() );
-    }
+        typedef void result_type;
 
-    template< class StateOut , class StateIn >
-    void resize_op( StateOut &x1 , const StateIn &x2 , boost::true_type ) const
-    {
-        resize( x1 , x2 );
-    }
+        template< class StateOut , class StateIn >
+        void operator()( StateOut &x1 , const StateIn &x2 ) const
+        {
+            resize_op( x1 , x2 , typename is_resizeable< StateOut >::type() );
+        }
 
-    template< class StateOut , class StateIn >
-    void resize_op( StateOut &x1 , const StateIn &x2 , boost::false_type ) const
-    {
-    }
+        template< class StateOut , class StateIn >
+        void resize_op( StateOut &x1 , const StateIn &x2 , boost::true_type ) const
+        {
+            resize( x1 , x2 );
+        }
 
-};
+        template< class StateOut , class StateIn >
+        void resize_op( StateOut &x1 , const StateIn &x2 , boost::false_type ) const
+        {
+        }
+
+    };
+} // nanespace detail
 
 
 /*
@@ -87,7 +91,7 @@ struct resize_impl< FusionSeq , FusionSeq , typename boost::enable_if< typename 
     {
         typedef boost::fusion::vector< FusionSeq& , const FusionSeq& > Sequences;
         Sequences sequences( x1 , x2 );
-        boost::fusion::for_each( boost::fusion::zip_view< Sequences >( sequences ) , boost::fusion::make_fused( resizer() ) );
+        boost::fusion::for_each( boost::fusion::zip_view< Sequences >( sequences ) , boost::fusion::make_fused( detail::resizer() ) );
     }
 };
 
