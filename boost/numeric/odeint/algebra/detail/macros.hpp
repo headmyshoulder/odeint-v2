dd88d5c6dd72097f05af9561adb02e27f7496d12
Denis Demidov <ddemidov@ksu.ru>
2012-10-26 17:33:18 +0400

    Reverted unintentional change of __CUDACC__ to DECORATE_CALLS

--- boost/numeric/odeint/algebra/detail/macros.hpp
+++ boost/numeric/odeint/algebra/detail/macros.hpp
@@ -20,7 +20,7 @@
 
 
 //type traits aren't working with nvcc
-#ifndef DECORATE_CALLS
+#ifndef __CUDACC__
 #include <boost/type_traits.hpp>
 #include <boost/static_assert.hpp>
 
@@ -31,7 +31,7 @@
 //empty macro for nvcc
 #define BOOST_ODEINT_CHECK_CONTAINER_TYPE( Type1 , Type2 )
 
-#endif // DECORATE_CALLS
+#endif // __CUDACC__
 
 
 
