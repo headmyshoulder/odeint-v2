0e897babfa0d18eeb8121f2ecee0c6c048cad498
Mario Mulansky <mario.mulansky@gmx.net>
2012-11-30 09:48:45 +0100

    deleted forwarding versions of integrate

--- boost/numeric/odeint/integrate/integrate.hpp
+++ boost/numeric/odeint/integrate/integrate.hpp
@@ -35,26 +35,12 @@ namespace odeint {
  *
  * determine type of dxdt for units
  *
- * the two overloads are needed in order to solve the forwarding problem
  */
 template< class System , class State , class Time , class Observer >
 size_t integrate( System system , State &start_state , Time start_time , Time end_time , Time dt , Observer observer )
 {
     return integrate_adaptive( controlled_runge_kutta< runge_kutta_dopri5< State > >() , system , start_state , start_time , end_time , dt , observer );
 }
-
-/**
- * \brief Overload to solve the forwarding problem.
- */
-template< class System , class State , class Time , class Observer >
-size_t integrate( System system , const State &start_state , Time start_time , Time end_time , Time dt , Observer observer )
-{
-    return integrate_adaptive( controlled_runge_kutta< runge_kutta_dopri5< State > >() , system , start_state , start_time , end_time , dt , observer );
-}
-
-
-
-
 
 
 
@@ -66,17 +52,6 @@ size_t integrate( System system , State &start_state , Time start_time , Time en
 {
     return integrate( system , start_state , start_time , end_time , dt , null_observer() );
 }
-
-/**
- * \brief Overload to solve the forwarding problem.
- */
-template< class System , class State , class Time >
-size_t integrate( System system , const State &start_state , Time start_time , Time end_time , Time dt )
-{
-    return integrate( system , start_state , start_time , end_time , dt , null_observer() );
-}
-
-
 
 
 /**
