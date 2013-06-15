ce30759861e8e0c9838c4e8483e022988510bd85
Pascal Germroth <pascal@ensieve.org>
2013-06-15 23:03:59 +0200

    Clang complains about unnecessary typenames

--- libs/numeric/odeint/test/algebra_dispatcher.cpp
+++ libs/numeric/odeint/test/algebra_dispatcher.cpp
@@ -42,31 +42,31 @@ BOOST_AUTO_TEST_SUITE( algebra_dispatcher_test )
 BOOST_AUTO_TEST_CASE( range_algebra_with_vector )
 {
     typedef runge_kutta4< std::vector< double > > stepper_type;
-    BOOST_STATIC_ASSERT(( boost::is_same< typename stepper_type::algebra_type , range_algebra >::value ));
+    BOOST_STATIC_ASSERT(( boost::is_same< stepper_type::algebra_type , range_algebra >::value ));
 }
 
 BOOST_AUTO_TEST_CASE( array_algebra_with_array )
 {
     typedef runge_kutta4< boost::array< double , 2 > > stepper_type;
-    BOOST_STATIC_ASSERT(( boost::is_same< typename stepper_type::algebra_type , array_algebra >::value ));
+    BOOST_STATIC_ASSERT(( boost::is_same< stepper_type::algebra_type , array_algebra >::value ));
 }
 
 BOOST_AUTO_TEST_CASE( range_algebra_with_array )
 {
     typedef runge_kutta4< boost::array< double , 2 > , double , boost::array< double , 2 > , double , range_algebra > stepper_type;
-    BOOST_STATIC_ASSERT(( boost::is_same< typename stepper_type::algebra_type , range_algebra >::value ));
+    BOOST_STATIC_ASSERT(( boost::is_same< stepper_type::algebra_type , range_algebra >::value ));
 }
 
 BOOST_AUTO_TEST_CASE( fusion_algebra_with_fusion_vector )
 {
     typedef runge_kutta4< boost::fusion::vector< double > > stepper_type;
-    BOOST_STATIC_ASSERT(( boost::is_same< typename stepper_type::algebra_type , fusion_algebra >::value ));
+    BOOST_STATIC_ASSERT(( boost::is_same< stepper_type::algebra_type , fusion_algebra >::value ));
 }
 
 BOOST_AUTO_TEST_CASE( fusion_algebra_with_fusion_vector2 )
 {
     typedef runge_kutta_fehlberg78< boost::fusion::vector< double > > stepper_type;
-    BOOST_STATIC_ASSERT(( boost::is_same< typename stepper_type::algebra_type , fusion_algebra >::value ));
+    BOOST_STATIC_ASSERT(( boost::is_same< stepper_type::algebra_type , fusion_algebra >::value ));
 }
 
 typedef boost::mpl::list< float , double , long double , std::complex< double > , std::complex< float > > fp_types;
