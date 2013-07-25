4688e3a0f042cb4f6af7760c996f177664c0019c
Karsten Ahnert <karsten.ahnert@gmx.de>
2013-07-25 17:16:13 +0200

    continue with velocity verlet

--- boost/numeric/odeint/stepper/velocity_verlet.hpp
+++ boost/numeric/odeint/stepper/velocity_verlet.hpp
@@ -34,26 +34,29 @@
 // #include <boost/array.hpp>
 
 
-namespace boost {
-namespace numeric {
-namespace odeint {
+namespace boost
+{
+namespace numeric
+{
+namespace odeint
+{
 
-template<
-    class Coor ,
-    class Momentum = Coor ,
-    class Value = double ,
-    class CoorDeriv = Coor ,
-    class MomentumDeriv = Coor ,
-    class Time = Value ,
-    class TimeSq = Time ,
-    class Algebra = typename algebra_dispatcher< Coor >::algebra_type ,
-    class Operations = typename operations_dispatcher< Coor >::operations_type ,
-    class Resizer = initially_resizer
->
+template <
+class Coor ,
+      class Momentum = Coor ,
+      class Value = double ,
+      class CoorDeriv = Coor ,
+      class MomentumDeriv = Coor ,
+      class Time = Value ,
+      class TimeSq = Time ,
+      class Algebra = typename algebra_dispatcher< Coor >::algebra_type ,
+      class Operations = typename operations_dispatcher< Coor >::operations_type ,
+      class Resizer = initially_resizer
+      >
 class velocity_verlet : public algebra_stepper_base< Algebra , Operations >
 {
 public:
-    
+
     typedef algebra_stepper_base< Algebra , Operations > algebra_stepper_base_type;
     typedef typename algebra_stepper_base_type::algebra_type algebra_type;
     typedef typename algebra_stepper_base_type::operations_type operations_type;
@@ -63,7 +66,7 @@ public:
     typedef std::pair< coor_type , momentum_type > state_type;
     typedef CoorDeriv coor_deriv_type;
     typedef MomentumDeriv momentum_deriv_type;
-    typedef std::pair< coor_deriv_type , momentum_deriv_type > deriv_type;    
+    typedef std::pair< coor_deriv_type , momentum_deriv_type > deriv_type;
     typedef state_wrapper< coor_deriv_type> wrapped_coor_deriv_type;
     typedef state_wrapper< momentum_deriv_type > wrapped_momentum_deriv_type;
     typedef Value value_type;
@@ -71,57 +74,56 @@ public:
     typedef TimeSq time_square_type;
     typedef Resizer resizer_type;
     typedef stepper_tag stepper_category;
-    
+
     typedef unsigned short order_type;
 
     static const order_type order_value = 1;
-    
-    order_type order( void ) const
-    {
+
+    order_type order( void ) const {
         return order_value;
     }
 
-    
-    velocity_verlet( const algebra_type &algebra = algebra_type() )
-    : algebra_stepper_base_type( algebra ) , m_first_call( true ) { }
 
-    
-    template< class System , class StateIn >
-    void do_step( System system , StateIn &x , time_type t , time_type dt )
+    velocity_verlet( const algebra_type & algebra = algebra_type() )
+        : algebra_stepper_base_type( algebra ) , m_first_call( true )
+        , m_a1() , m_a2() , m_current_a1( true ) { }
+
+
+    template< class System , class StateInOut >
+    void do_step( System system , StateInOut & x , time_type t , time_type dt )
     {
-        typedef typename odeint::unwrap_reference< StateIn >::type state_in_type;
-        typedef typename odeint::unwrap_reference< typename state_in_type::first_type >::type coor_in_type;
-        typedef typename odeint::unwrap_reference< typename state_in_type::second_type >::type momentum_in_type;
-        const state_in_type &statein = in;
-        const coor_in_type &qinout = statein.first;
-        const momentum_in_type &pinout = statein.second;
-        
-        // alloc a
-
-        do_step( system , qinout , pinout , m_a , qinout , pinout , m_a , t , dt );
-
+        do_step_v1( system , x , t , dt );
     }
     
+    template< class System , class StateInOut >
+    void do_step( System system , const StateInOut & x , time_type t , time_type dt )
+    {
+        do_step_v1( system , x , t , dt );
+    }
+
     template< class System , class CoorIn , class MomentumIn , class MomentumDerivIn , class CoorOut , class MomentumOut , class MomentumDerivOut >
-    void do_step( System system , CoorIn const& qin , MomentumIn const& pin , MomentumDerivIn const& ain , CoorOut &qout , MomentumOut &pout , MomentumDerivOut &aout , time_type t , time_type dt )
+    void do_step( System system , CoorIn const & qin , MomentumIn const & pin , MomentumDerivIn const & ain ,
+                  CoorOut & qout , MomentumOut & pout , MomentumDerivOut & aout , time_type t , time_type dt )
     {
         const value_type one = static_cast< value_type >( 1.0 );
         const value_type one_half = static_cast< value_type >( 0.5 );
-        
-        algebra_stepper_base_type::for_each4( qout , qin , pin , ain , 
-                                              typename operations_type::template scale_sum3< value_type , time_type , time_square_type >( one , one_half * dt , one * dt * dt ) );
-        
-        typename odeint::unwrap_reference< System >::type &sys = system;
-        
+
+        algebra_stepper_base_type::m_algebra.for_each4(
+            qout , qin , pin , ain ,
+            typename operations_type::template scale_sum3< value_type , time_type , time_square_type >( one , one_half * dt , one * dt * dt ) );
+
+        typename odeint::unwrap_reference< System >::type & sys = system;
+
         sys( qout , pin , aout );
-        
-        algebra_stepper_base_type::for_each4( pout , pin , ain , aout ,
-                                              typename operations_type::template scale_sum3< value_type , time_type , time_type >( one , one_half * dt , one_half * dt ) );
+
+        algebra_stepper_base_type::m_algebra.for_each4(
+            pout , pin , ain , aout ,
+            typename operations_type::template scale_sum3< value_type , time_type , time_type >( one , one_half * dt , one_half * dt ) );
     }
-    
-  
+
+
     template< class StateIn >
-    void adjust_size( const StateIn &x )
+    void adjust_size( const StateIn & x )
     {
         resize_impl( x );
     }
@@ -132,17 +134,17 @@ public:
     }
 
     template< class MomentumDerivIn >
-    void initialize( const MomentumDerivIn &ain )
+    void initialize( const MomentumDerivIn & ain )
     {
-        boost::numeric::odeint::copy( ain , m_a.m_v );
+        boost::numeric::odeint::copy( ain , get_current_acc() );
         m_first_call = false;
     }
 
-    template< class System , class StateIn >
-    void initialize( System system , const CoorIn &qin , const MomentumIn &pin )
+    template< class System , class CoorIn , class MomentumIn >
+    void initialize( System system , const CoorIn & qin , const MomentumIn & pin )
     {
-        typename odeint::unwrap_reference< System >::type &sys = system;
-        sys( qin , pin , m_a.m_v );
+        typename odeint::unwrap_reference< System >::type & sys = system;
+        sys( qin , pin , get_current_acc() );
         m_first_call = false;
     }
 
@@ -154,16 +156,67 @@ public:
 
 private:
     
-    template< class StateIn >
-    bool resize_impl( const StateIn &x )
+    template< class System , class StateInOut >
+    void do_step_v1( System system , StateInOut & x , time_type t , time_type dt )
     {
-        return adjust_size_by_resizeability( m_a , x , typename is_resizeable< momentum_deriv_type >::type() );
+        typedef typename odeint::unwrap_reference< StateInOut >::type state_in_type;
+        typedef typename odeint::unwrap_reference< typename state_in_type::first_type >::type coor_in_type;
+        typedef typename odeint::unwrap_reference< typename state_in_type::second_type >::type momentum_in_type;
+        
+        typedef typename boost::remove_reference< coor_in_type >::type xyz_type;
+        state_in_type & statein = x;
+        coor_in_type & qinout = statein.first;
+        momentum_in_type & pinout = statein.second;
+
+        // alloc a
+        if( m_resizer.adjust_size( qinout ,
+                                   detail::bind( &velocity_verlet::template resize_impl< xyz_type > ,
+                                                 detail::ref( *this ) , detail::_1 ) )
+         || m_first_call )
+        {
+            initialize( system , qinout , pinout );
+        }
+
+        // check first
+        do_step( system , qinout , pinout , get_current_acc() , qinout , pinout , get_old_acc() , t , dt );
+        toggle_current_acc();
     }
 
+    template< class StateIn >
+    bool resize_impl( const StateIn & x )
+    {
+        return adjust_size_by_resizeability( m_a1 , x , typename is_resizeable< momentum_deriv_type >::type() )
+            || adjust_size_by_resizeability( m_a2 , x , typename is_resizeable< momentum_deriv_type >::type() );
+    }
+
+    momentum_deriv_type & get_current_acc( void )
+    {
+        return m_current_a1 ? m_a1.m_v : m_a2.m_v ;
+    }
+
+    const momentum_deriv_type & get_current_acc( void ) const
+    {
+        return m_current_a1 ? m_a1.m_v : m_a2.m_v ;
+    }
+
+    momentum_deriv_type & get_old_acc( void )
+    {
+        return m_current_a1 ? m_a2.m_v : m_a1.m_v ;
+    }
+
+    const momentum_deriv_type & get_old_acc( void ) const
+    {
+        return m_current_a1 ? m_a2.m_v : m_a1.m_v ;
+    }
+
+    void toggle_current_acc( void ) {
+        m_current_a1 = ! m_current_a1;
+    }
 
     resizer_type m_resizer;
     bool m_first_call;
-    wrapped_momentum_deriv_type m_a;
+    wrapped_momentum_deriv_type m_a1 , m_a2;
+    bool m_current_a1;
 };
 
 
--- libs/numeric/odeint/test/velocity_verlet.cpp
+++ libs/numeric/odeint/test/velocity_verlet.cpp
@@ -25,19 +25,81 @@
 
 #include <boost/array.hpp>
 #include <boost/test/unit_test.hpp>
+#include "../../../../../../boost/boost_1_54_0/boost/concept_check.hpp"
 
 
 
 using namespace boost::unit_test;
 using namespace boost::numeric::odeint;
 
+struct ode
+{
+    template< class CoorIn , class MomentumIn , class AccelerationOut >
+    void operator()( const CoorIn &q , const MomentumIn &p , AccelerationOut &a ) const
+    {
+        a[0] = -q[0] - p[0];
+        a[1] = -q[1] - p[1];
+    }
+};
+
+template< class Q , class P >
+void init_state( Q &q , P &p )
+{
+    q[0] = 1.0 ; q[1] = 0.5;
+    p[0] = 2.0 ; p[1] = -1.0;
+}
+
+typedef boost::array< double , 2 > array_type;
+typedef std::vector< double > vector_type;
+
+typedef velocity_verlet< array_type > array_stepper;
+typedef velocity_verlet< vector_type > vector_stepper;
 
 BOOST_AUTO_TEST_SUITE( velocity_verlet_test )
 
-BOOST_AUTO_TEST_CASE( test1 )
+BOOST_AUTO_TEST_CASE( test_with_array_ref )
 {
-    typedef boost::array< double , 2 > state_type;
-    velocity_verlet< state_type > stepper;
+    array_stepper stepper;
+    array_type q , p ;
+    init_state( q , p );
+    stepper.do_step( ode() , std::make_pair( boost::ref( q ) , boost::ref( p ) ) , 0.0 , 0.01 );
 }
+
+BOOST_AUTO_TEST_CASE( test_with_array_pair )
+{
+    array_stepper stepper;
+    std::pair< array_type , array_type > xxx;
+    init_state( xxx.first , xxx.second );
+    stepper.do_step( ode() , xxx , 0.0 , 0.01 );
+}
+
+BOOST_AUTO_TEST_CASE( test_with_vector_ref )
+{
+    vector_stepper stepper;
+    vector_type q( 2 ) , p( 2 );
+    init_state( q , p );
+    stepper.do_step( ode() , std::make_pair( boost::ref( q ) , boost::ref( p ) ) , 0.0 , 0.01 );
+}
+
+BOOST_AUTO_TEST_CASE( test_with_vector_pair )
+{
+    vector_stepper stepper;
+    std::pair< vector_type , vector_type > x;
+    init_state( x.first , x.second );
+    stepper.do_step( ode() , x , 0.0 , 0.01 );
+}
+
+BOOST_AUTO_TEST_CASE( test_reset )
+{
+}
+
+BOOST_AUTO_TEST_CASE( test_initialize )
+{
+}
+
+BOOST_AUTO_TEST_CASE( test_resize )
+{
+}
+
 
 BOOST_AUTO_TEST_SUITE_END()
