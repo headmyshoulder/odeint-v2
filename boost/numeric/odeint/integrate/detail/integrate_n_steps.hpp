973e908a2760b0ee59cc99dab57d4545be8b754d
mariomulansky <mario.mulansky@gmx.net>
2013-12-15 22:45:20 +0100

    another todo...

--- boost/numeric/odeint/integrate/detail/integrate_n_steps.hpp
+++ boost/numeric/odeint/integrate/detail/integrate_n_steps.hpp
@@ -50,6 +50,7 @@ Time integrate_n_steps(
     typedef typename odeint::unwrap_reference< Observer >::type observer_type;
     observer_type &obs = observer;
 
+    // ToDo: is there a better way to extract the final time?
     Time t;
     boost::for_each( make_n_step_time_range( stepper , system , start_state ,
                                              start_time , dt , num_of_steps ) ,
