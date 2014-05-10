85bedc8ffb69b204f2bdd905c55994805c1e3725
Karsten Ahnert <karsten.ahnert@gmx.de>
2014-05-10 10:30:13 +0200

    adding range performance tests for vectors

--- performance/ranges/const_step_range_vector.cpp
+++ performance/ranges/const_step_range_vector.cpp
@@ -14,7 +14,7 @@
  copy at http://www.boost.org/LICENSE_1_0.txt)
  */
 
-#include <phase_lattice.hpp>
+
 #include "timer.hpp"
 #include "helpers.hpp"
 
@@ -32,10 +32,39 @@
 
 namespace odeint = boost::numeric::odeint;
 
-// Only for arrays
+using value_type = double;
+using state_type = std::vector< value_type >;
 
-#include <chrono>
 
+struct phase_lattice
+{
+    size_t m_N;
+    value_type m_epsilon; 
+    state_type m_omega;
+
+
+    phase_lattice( size_t N )
+        : m_N( N ) , m_epsilon( 6.0/(N*N) ) , m_omega( N ) // should be < 8/N^2 to see phase locking
+    {
+        for( size_t i=0 ; i<m_N ; ++i )
+            m_omega[i] = m_epsilon*(m_N-i);
+    }
+
+    void inline operator()( const state_type &x , state_type &dxdt , const double t ) const
+    {
+        double c = 0.0;
+
+        for( size_t i=0 ; i<m_N-1 ; ++i )
+        {
+            dxdt[i] = m_omega[i] + c;
+            c = x[i+1] - x[i];
+            dxdt[i] += c;
+        }
+
+        dxdt[m_N-1] = m_omega[m_N-1] + x[m_N-1] - x[m_N-2];
+    }
+
+};
 
 
 
@@ -44,15 +73,12 @@ struct test_range
 {
     static double run( void )
     {
-        typedef phase_lattice< N > system_type;
-        typedef typename system_type::state_type state_type;
-
-        state_type x;
+        state_type x( N );
         ic( x );
         boost::for_each( odeint::make_const_step_range(
             odeint::runge_kutta4< state_type > {} ,
             x ,
-            phase_lattice< N > {} , 
+            phase_lattice { N } , 
             0.0 , 100.0 , 0.01 ) , empty_observer {} );
         return x[0];
     }
@@ -63,19 +89,16 @@ struct test_iterator
 {
     static double run( void )
     {
-        typedef phase_lattice< N > system_type;
-        typedef typename system_type::state_type state_type;
-
-        state_type x;
+        state_type x( N );
         ic( x );
         std::for_each(
             odeint::make_const_step_iterator_begin(
                 odeint::runge_kutta4< state_type > {} ,
-                phase_lattice< N > {} , 
+                phase_lattice { N } , 
                 x , 0.0 , 100.0 , 0.01 ) ,
             odeint::make_const_step_iterator_end(
                 odeint::runge_kutta4< state_type > {} ,
-                phase_lattice< N > {} ,
+                phase_lattice { N } ,
                 x ) ,                
             empty_observer {} );
         return x[0];
@@ -88,15 +111,12 @@ struct test_iterator_range
 {
     static double run( void )
     {
-        typedef phase_lattice< N > system_type;
-        typedef typename system_type::state_type state_type;
-
-        state_type x;
+        state_type x( N );
         ic( x );
         boost::for_each(
             odeint::make_const_step_range(
                 odeint::runge_kutta4< state_type > {} ,
-                phase_lattice< N > {} , 
+                phase_lattice { N } , 
                 x , 0.0 , 100.0 , 0.01 ) ,
             empty_observer {} );
         return x[0];
@@ -108,15 +128,12 @@ struct test_n_step_iterator_range
 {
     static double run( void )
     {
-        typedef phase_lattice< N > system_type;
-        typedef typename system_type::state_type state_type;
-
-        state_type x;
+        state_type x( N );
         ic( x );
         boost::for_each(
             odeint::make_n_step_range(
                 odeint::runge_kutta4< state_type > {} ,
-                phase_lattice< N > {} , 
+                phase_lattice { N } , 
                 x , 0.0 , 0.01 , 10000 ) ,
             empty_observer {} );
         return x[0];
@@ -129,16 +146,13 @@ struct test_raw
 {
     static double run( void )
     {
-        typedef phase_lattice< N > system_type;
-        typedef typename system_type::state_type state_type;
-
-        state_type x;
+        state_type x( N );
         ic( x );
         double t = 0.0;
         odeint::runge_kutta4< state_type > stepper;
         while( t < 100.0 )
         {
-            stepper.do_step( phase_lattice< N > {} , x , t , 0.01 );
+            stepper.do_step( phase_lattice { N } , x , t , 0.01 );
             t += 0.01;
         }
         return x[0];
@@ -150,12 +164,9 @@ struct test_integrate
 {
     static double run( void )
     {
-        typedef phase_lattice< N > system_type;
-        typedef typename system_type::state_type state_type;
-
-        state_type x;
+        state_type x( N );
         ic( x );
-        odeint::integrate_const( odeint::runge_kutta4< state_type > {} , phase_lattice< N > {} ,
+        odeint::integrate_const( odeint::runge_kutta4< state_type > {} , phase_lattice { N } ,
                                  x , 0.0 , 100.0 , 0.01 , empty_observer {} );
         return x[0];
     }
@@ -184,16 +195,34 @@ struct test
 int main( int argc , char *argv[] )
 {
     std::cout.precision( 14 );
+    test< 3 >::run();
+    test< 4 >::run();
+    test< 5 >::run();
+    test< 6 >::run();
     test< 8 >::run();
+    test< 10 >::run();
+    test< 12 >::run();
+    test< 14 >::run();
     test< 16 >::run();
+    test< 20 >::run();
+    test< 24 >::run();
+    test< 28 >::run();
     test< 32 >::run();
+    test< 48 >::run();
     test< 64 >::run();
+    test< 96 >::run();
     test< 128 >::run();
+    test< 192 >::run();
     test< 256 >::run();
+    test< 384 >::run();
     test< 512 >::run();
     test< 1024 >::run();
     test< 2048 >::run();
     test< 4096 >::run();
+    test< 8192 >::run();
+    test< 16384 >::run();
+    test< 32768 >::run();
+    test< 65536 >::run();
     
     return 0;
 }
