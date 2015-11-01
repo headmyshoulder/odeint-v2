60cd09818df69cd6eeeee5334f082bdd893bab52
Mario Mulansky <mario.mulansky@gmx.net>
2015-11-01 14:21:02 -0600

    add max_dt to rosenbrock steppers
    now all controlled and dense out steppers have step size limitation
    functionality.

--- test/step_size_limitation.cpp
+++ test/step_size_limitation.cpp
@@ -29,6 +29,10 @@ using namespace boost::numeric::odeint;
 typedef double value_type;
 typedef std::vector< value_type > state_type;
 
+/***********************************************
+ * first part of the tests: explicit methods
+ ***********************************************
+ */
 
 void damped_osc( const state_type &x , state_type &dxdt , const value_type t )
 {
@@ -46,7 +50,8 @@ struct push_back_time
     push_back_time( std::vector< double > &times )
     :  m_times( times ) { }
 
-    void operator()( const state_type &x , double t )
+    template<typename State>
+    void operator()( const State &x , double t )
     {
         m_times.push_back( t );
     }
@@ -90,7 +95,7 @@ BOOST_AUTO_TEST_CASE( test_step_adjuster )
 
     dt_new = limited_adjuster.increase_step(dt, 0.4, 1);
     // but even for smaller errors, we should at most get 0.1
-    BOOST_CHECK(dt_new == dt);
+    BOOST_CHECK_EQUAL(dt_new, dt);
 
     dt_new = limited_adjuster.increase_step(0.9*dt, 0.1, 1);
     std::cout << dt_new << std::endl;
@@ -99,27 +104,28 @@ BOOST_AUTO_TEST_CASE( test_step_adjuster )
 }
 
 
-template<class ControlledStepper>
-void test_controlled_stepper(ControlledStepper stepper, const double max_dt)
+template<class Stepper>
+void test_explicit_stepper(Stepper stepper, const double max_dt)
 {
     state_type x( 2 );
     x[0] = x[1] = 10.0;
+    const size_t steps = 100;
 
     std::vector<double> times;
 
-    integrate_adaptive(stepper, damped_osc, x, 0.0, 100*max_dt, max_dt, push_back_time(times));
+    integrate_adaptive(stepper, damped_osc, x, 0.0, steps*max_dt, max_dt, push_back_time(times));
+
+    BOOST_CHECK_EQUAL(times.size(), steps+1);
     // check that dt remains at exactly max_dt
-
-    std::cout << "STEP: " << times.size() << " , END: " << times[times.size()-1] << std::endl;
-
     for( size_t i=0 ; i<times.size() ; ++i )
         // check if observer was called at times 0,1,2,...
         BOOST_CHECK_SMALL( times[i] - static_cast<double>(i)*max_dt , 1E-15);
     times.clear();
 
-    return;
     // this should also work when we provide some bigger initial dt
-    integrate_adaptive(stepper, damped_osc, x, 0.0, 100*max_dt, 10*max_dt, push_back_time(times));
+    integrate_adaptive(stepper, damped_osc, x, 0.0, steps*max_dt, 10*max_dt, push_back_time(times));
+
+    BOOST_CHECK_EQUAL(times.size(), steps+1);
     // check that dt remains at exactly max_dt
     for( size_t i=0 ; i<times.size() ; ++i )
         // check if observer was called at times 0,1,2,...
@@ -132,41 +138,92 @@ BOOST_AUTO_TEST_CASE( test_controlled )
 {
     const double max_dt = 0.01;
 
-    test_controlled_stepper(make_controlled(1E-2, 1E-2, max_dt,
+    test_explicit_stepper(make_controlled(1E-2, 1E-2, max_dt,
                                             runge_kutta_dopri5<state_type>()),
                             max_dt);
-    test_controlled_stepper(make_controlled(1E-2, 1E-2, -max_dt,
+    test_explicit_stepper(make_controlled(1E-2, 1E-2, -max_dt,
                                             runge_kutta_dopri5<state_type>()),
                             -max_dt);
-    std::cout << "DOPRI5 FINISHED" << std::endl;
 
-    test_controlled_stepper(make_controlled(1E-2, 1E-2, max_dt,
+    test_explicit_stepper(make_controlled(1E-2, 1E-2, max_dt,
                                             runge_kutta_cash_karp54<state_type>()),
                             max_dt);
-    test_controlled_stepper(make_controlled(1E-2, 1E-2, -max_dt,
+    test_explicit_stepper(make_controlled(1E-2, 1E-2, -max_dt,
                                             runge_kutta_cash_karp54<state_type>()),
                             -max_dt);
-    std::cout << "RK-CK54 FINISHED" << std::endl;
 
-    test_controlled_stepper(bulirsch_stoer<state_type>(1E-2, 1E-2, 1.0, 1.0, max_dt),
+    test_explicit_stepper(bulirsch_stoer<state_type>(1E-2, 1E-2, 1.0, 1.0, max_dt),
                             max_dt);
-    test_controlled_stepper(bulirsch_stoer<state_type>(1E-2, 1E-2, 1.0, 1.0, -max_dt),
+    test_explicit_stepper(bulirsch_stoer<state_type>(1E-2, 1E-2, 1.0, 1.0, -max_dt),
                             -max_dt);
-    std::cout << "BS FINISHED" << std::endl;
 }
-
 
 
 BOOST_AUTO_TEST_CASE( test_dense_out )
 {
-    state_type x( 2 );
-    x[0] = x[1] = 10.0;
     const double max_dt = 0.01;
+    test_explicit_stepper(make_dense_output(1E-2, 1E-2, max_dt,
+                                             runge_kutta_dopri5<state_type>()),
+                          max_dt);
+    test_explicit_stepper(make_dense_output(1E-2, 1E-2, -max_dt,
+                                            runge_kutta_dopri5<state_type>()),
+                          -max_dt);
+
+    test_explicit_stepper(bulirsch_stoer_dense_out<state_type>(1E-2, 1E-2, 1, 1, max_dt),
+                          max_dt);
+
+    test_explicit_stepper(bulirsch_stoer_dense_out<state_type>(1E-2, 1E-2, 1, 1, -max_dt),
+                          -max_dt);
+}
+
+
+/***********************************************
+ * second part of the tests: implicit Rosenbrock
+ ***********************************************
+ */
+
+typedef boost::numeric::ublas::vector< value_type > vector_type;
+typedef boost::numeric::ublas::matrix< value_type > matrix_type;
+
+
+// harmonic oscillator, analytic solution x[0] = sin( t )
+struct osc_rhs
+{
+    void operator()( const vector_type &x , vector_type &dxdt , const value_type &t ) const
+    {
+        dxdt( 0 ) = x( 1 );
+        dxdt( 1 ) = -x( 0 );
+    }
+};
+
+struct osc_jacobi
+{
+    void operator()( const vector_type &x , matrix_type &jacobi , const value_type &t , vector_type &dfdt ) const
+    {
+        jacobi( 0 , 0 ) = 0;
+        jacobi( 0 , 1 ) = 1;
+        jacobi( 1 , 0 ) = -1;
+        jacobi( 1 , 1 ) = 0;
+        dfdt( 0 ) = 0.0;
+        dfdt( 1 ) = 0.0;
+    }
+};
+
+
+template<class Stepper>
+void test_rosenbrock_stepper(Stepper stepper, const double max_dt)
+{
+    vector_type x( 2 );
+    x(0) = x(1) = 10.0;
+    const size_t steps = 100;
 
     std::vector<double> times;
 
-    integrate_adaptive(make_dense_output(1E-2, 1E-2, max_dt, runge_kutta_dopri5<state_type>()),
-                       damped_osc, x, 0.0, 1.0, max_dt, push_back_time(times));
+    integrate_adaptive(stepper,
+                       std::make_pair(osc_rhs(), osc_jacobi()),
+                       x, 0.0, steps*max_dt, max_dt, push_back_time(times));
+
+    BOOST_CHECK_EQUAL(times.size(), steps+1);
     // check that dt remains at exactly max_dt
     for( size_t i=0 ; i<times.size() ; ++i )
         // check if observer was called at times 0,1,2,...
@@ -174,13 +231,38 @@ BOOST_AUTO_TEST_CASE( test_dense_out )
     times.clear();
 
     // this should also work when we provide some bigger initial dt
-    integrate_adaptive(make_dense_output(1E-2, 1E-2, max_dt, runge_kutta_dopri5<state_type>()),
-                       damped_osc, x, 0.0, 1.0, 0.1, push_back_time(times));
+    integrate_adaptive(stepper,
+                       std::make_pair(osc_rhs(), osc_jacobi()),
+                       x, 0.0, steps*max_dt, 10*max_dt, push_back_time(times));
+
+    BOOST_CHECK_EQUAL(times.size(), steps+1);
     // check that dt remains at exactly max_dt
     for( size_t i=0 ; i<times.size() ; ++i )
         // check if observer was called at times 0,1,2,...
         BOOST_CHECK_SMALL( times[i] - static_cast<double>(i)*max_dt , 1E-15);
     times.clear();
+}
+
+
+BOOST_AUTO_TEST_CASE( test_controlled_rosenbrock )
+{
+    const double max_dt = 0.01;
+
+    test_rosenbrock_stepper(make_controlled(1E-2, 1E-2, max_dt, rosenbrock4<value_type>()),
+                            max_dt);
+    test_rosenbrock_stepper(make_controlled(1E-2, 1E-2, -max_dt, rosenbrock4<value_type>()),
+                            -max_dt);
+}
+
+
+BOOST_AUTO_TEST_CASE( test_dense_out_rosenbrock )
+{
+    const double max_dt = 0.01;
+
+    test_rosenbrock_stepper(make_dense_output(1E-2, 1E-2, max_dt, rosenbrock4<value_type>()),
+                            max_dt);
+    test_rosenbrock_stepper(make_dense_output(1E-2, 1E-2, -max_dt, rosenbrock4<value_type>()),
+                            -max_dt);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
