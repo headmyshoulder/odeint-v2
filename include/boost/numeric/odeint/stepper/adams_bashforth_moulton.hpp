1eefa78c96a0f103407e956d513cf2387f5899fa
Mario Mulansky <mario.mulansky@gmx.net>
2015-12-09 12:09:39 +0100

    Bugfix for #144 also in const version
    Issue #144 has been fixed some time ago, but only for the non-const ref do_step
    interface. This fixes also the const ref interface implementation.

--- include/boost/numeric/odeint/stepper/adams_bashforth_moulton.hpp
+++ include/boost/numeric/odeint/stepper/adams_bashforth_moulton.hpp
@@ -175,7 +175,7 @@ private:
         {
             m_resizer.adjust_size( in , detail::bind( &stepper_type::template resize_impl< StateInOut > , detail::ref( *this ) , detail::_1 ) );        
             m_adams_bashforth.do_step( system , in , t , m_x.m_v , dt );
-            m_adams_moulton.do_step( system , in , m_x.m_v , t , out , dt , m_adams_bashforth.step_storage() );
+            m_adams_moulton.do_step( system , in , m_x.m_v , t+dt , out , dt , m_adams_bashforth.step_storage() );
         }
         else
         {
